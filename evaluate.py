bb3c12bf13f8bc9176c1dc7d198e90a384830045
minhanh01bg <vu.anh147852@gmail.com>
2022-12-24 07:04:14 +0700

    update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -30,7 +30,7 @@ def created_folder(path):
     except:
         return False
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         id_ = request.form.get('id')
@@ -84,7 +84,9 @@ def evaluate():
     try:
         id_ = str(request.form.get('id'))
         server = str(request.form.get('server')) + "_"
-        if id_ == '5dddd639-2c48-4e77-8c15-28fdb3ebe0e7':
+        
+        # count person and count car
+        if id_ == '5dddd639-2c48-4e77-8c15-28fdb3ebe0e7' or id_ == '11b4cf78-f831-4a37-8cae-b21f96e5be0d':
             # create folder
             created_folder('./predict_csv/'+id_)
             # download file predict.csv
@@ -109,7 +111,7 @@ def evaluate():
             time_sum = 0
             for file in os.listdir('./trust_csv/'+id_):
                 check = evaluate_count_person('./trust_csv/'+id_+'/'+file,'./predict_csv/'+id_+'/'+file)
-                print(check)
+                # print(check)
                 if check != True:
                     return jsonify({'error':check})
                 
@@ -146,21 +148,73 @@ def evaluate():
             return jsonify(JSON)
         
         # count car
-        elif id_== '11b4cf78-f831-4a37-8cae-b21f96e5be0d':
-            created_folder('./predict_csv/2')
-            for file in os.listdir('./trust_csv/1'):
-                df = pd.read_csv('https://kube-connect.zcode.vn/result_file/server01_'+file)
-                df.to_csv('./predict_csv/2/'+file,index=False)
-            evaluate_count_person('1.csv','1 copy.csv')
-            return jsonify(evaluate_end_point('evaluate_count_person.csv'))
+        # elif id_== '11b4cf78-f831-4a37-8cae-b21f96e5be0d':
+        #     created_folder('./predict_csv/2')
+        #     for file in os.listdir('./trust_csv/1'):
+        #         df = pd.read_csv('https://kube-connect.zcode.vn/result_file/server01_'+file)
+        #         df.to_csv('./predict_csv/2/'+file,index=False)
+        #     evaluate_count_person('1.csv','1 copy.csv')
+        #     return jsonify(evaluate_end_point('evaluate_count_person.csv'))
         
         # object detection
         elif id_== 'ec0cb7f9-9a70-47d0-9c4d-f84f124c3ecb':
-            created_folder('./predict_csv/3')
-            for file in os.listdir('./trust_csv/1'):
-                df = pd.read_csv('https://kube-connect.zcode.vn/result_file/server01_'+file)
-                df.to_csv('./predict_csv/3/'+file,index=False)
-            return jsonify(evaluate_detect_object('ob.csv','ob.csv'))
+            # create folder
+            created_folder('./predict_csv/'+id_)
+            # download file predict.csv
+            for file in os.listdir('./trust_csv/'+id_):
+                url = 'https://kube-connect.zcode.vn/result_file/' + server + file
+                header = {
+                "Authorization": "token 6b31ae0a90ca4ea9a6b8911cf0d9ad7d",
+                "Content-Type": "application/json",
+                "Accept":"application/json, text/plain, */*"
+                }
+                r = requests.get(url, headers=header)
+
+                url_content = r.content
+                csv_file = open('./predict_csv/'+id_+'/'+file, 'wb')
+                csv_file.write(url_content)
+                csv_file.close()
+            
+            # evaluate all file and return json 
+            pc_ = []
+            time_arr = []
+            memory_arr = []
+            time_sum = 0
+            for file in os.listdir('./trust_csv/'+id_):
+                # evaluation predicted video
+                percent_correct,memory = evaluate_detect_object('./trust_csv/'+id_+'/'+file,'./predict_csv/'+id_+'/'+file)
+                if percent_correct == 'KeyError':
+                    return jsonify({'error':memory})
+                # accuracy
+                pc_.append(percent_correct)
+                # memory
+                memory_arr.append(memory)
+                # time
+                time_str = str(datetime.datetime.now() - time_current)
+                time_ = datetime.time(int(time_str.split(':')[0]),int(time_str.split(':')[1]),int(time_str.split(':')[2].split('.')[0]))
+                # time_format = str(time_.hour) +'h'+ str(time_.minute) +'m'+ str(time_.second) +'s'
+                time_sum = time_sum + to_seconds(time_.hour,time_.minute,time_.second)
+                time_arr.append(to_seconds(time_.hour,time_.minute,time_.second))
+                time_current = datetime.datetime.now()
+            
+            # create json
+            ob_json_ =[]
+            for i in range(len(pc_)):
+                json_ = {
+                    'accuracy': pc_[i],
+                    'time': time_arr[i],
+                    'memory': memory_arr[i]
+                }
+                ob_json_.append(json_)
+            # average => json
+            summary = {
+                'accuracy': sum(pc_)/len(pc_),
+                'memory': sum(memory_arr)/len(memory_arr),
+                'time': time_sum / len(time_arr)
+            }
+            JSON = {'detail':ob_json_,'summary':summary}
+            return jsonify(JSON)
+            # return jsonify(evaluate_detect_object('ob.csv','ob.csv'))
         
         # license plate
         elif id_ == '6d6b2ede-18ba-4a60-8966-84566b4a826c':
--- python.py
+++ python.py
@@ -2,7 +2,8 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('name', help='Your name')
+    parser.add_argument('string',metavar='Name', help='Your name',type=str, nargs='?', default='Minh anh')
+    parser.add_argument('--name',action="store_true", default='Minh anh', help='Your name')
     args = parser.parse_args()
     print('Hello, {}'.format(args.name))
 
